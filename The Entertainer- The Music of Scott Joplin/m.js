5d588240f1fb219b202d702840dc26f0a4eda17e
littleflute <13699175164@sina.cn>
2017-06-15 16:05:24 -0500

    Add files via upload

---
+++ The Entertainer- The Music of Scott Joplin/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 Maple Leaf Rag.mp3";		
+f[2]	= "02 Solace.mp3";		
+f[3]	= "03 Magnetic Rag.mp3";		
+f[4]	= "04 The Entertainer.mp3";		
+f[5]	= "05 Eugenia.mp3";		
+f[6]	= "06 The Sycamore Rag.mp3";		
+f[7]	= "07 Search-Light Rag.mp3";		
+f[8]	= "08 The Nonpareil.mp3";		
+f[9]	= "09 Kismet Rag.mp3";		
+f[10]	= "10 Something Doing.mp3";		
+f[11]	= "11 Rosebud -- Two Step.mp3";		
+f[12]	= "16 Heliotrope Bouquet.mp3";		
